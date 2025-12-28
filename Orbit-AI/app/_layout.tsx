0ec3f5da5947116b91a592fab8691a5c5a29fa04
Siddhartha <siddureddyvr1115@email.com>
2025-12-28 09:06:11 +0530

    authentication and firebase setup

--- Orbit-AI/app/_layout.tsx
+++ Orbit-AI/app/_layout.tsx
@@ -1,9 +1,13 @@
 import { Stack } from "expo-router";
+import { ClerkProvider } from '@clerk/clerk-expo';
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
 
 export default function RootLayout() {
   return (
+   <ClerkProvider tokenCache={tokenCache} publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}>
        <Stack screenOptions={{headerShown:false}}>
           <Stack.Screen name="index"/>
        </Stack>
+   </ClerkProvider>
       )
 }
